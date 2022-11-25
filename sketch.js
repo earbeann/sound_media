7496b3b6c498b7e6fcf3e4564e9bbbbb8312a1b5
earbeann <118869506+earbeann@users.noreply.github.com>
2022-11-25 13:39:40 +0900

    동그라미 추가

--- sketch.js
+++ sketch.js
@@ -5,10 +5,16 @@ function setup()
     soundFormats('mp3', 'ogg');
     musicFile = loadSound('music.mp3', loadMusic);
     musicFile.play();
-   
+
 }
 
 function loadMusic()
 {
     musicFile.play();
+}
+
+function draw()
+{
+    fill(255,0,0);
+    ellipse(50,50,100,100);
 }
